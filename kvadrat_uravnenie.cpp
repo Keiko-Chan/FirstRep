29c5124f8474b89ea078113e0b0034ba6d61dc33
Ekaterina <70318220+Keiko-Chan@users.noreply.github.com>
2020-08-30 13:44:53 +0300

    Add files via upload

--- kvadrat_uravnenie.cpp
+++ kvadrat_uravnenie.cpp
@@ -3,17 +3,17 @@
 // It receives as input the coefficients of the quadratic equation  ( a, b, c )
 // and displays the roots ( x1, x2 ) of the equation and their number ( nroots ).
 
-#include<assert.h>
-#include<stdio.h>
-#include<math.h>
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
 
 const float bias = 1e-5;
 
 void test_solve(  );
 
-void test_isZero();
+void test_is_zero();
 
-int isZero( double Value );
+int is_zero( double Value );
 
 int solve_of_qua_equ( double a, double b, double c, double* x1, double* x2);
 
@@ -21,7 +21,7 @@ int solve_of_qua_equ( double a, double b, double c, double* x1, double* x2);
 
 int main()
   {
-  test_isZero();
+  test_is_zero();
   test_solve();
 
   printf( "Solution of a quadratic equation \n" );
@@ -38,9 +38,9 @@ int main()
 
   scanf( "%lg%lg%lg", &a, &b, &c );
 
-  assert( a != NULL );
-  assert( b != NULL );
-  assert( c != NULL );
+  assert( isfinite( a ) );
+  assert( isfinite( b ) );
+  assert( isfinite( c ) );
 
   nroots = solve_of_qua_equ( a, b, c, &x1, &x2);
 
@@ -75,11 +75,14 @@ int solve_of_qua_equ( double a, double b, double c, double* x1, double* x2)
    double Discr = 0;                       //discriminant of quadratic equation
    double Nroots = 0;                      //number of roots of quadratic equation
 
-   if( isZero( a ) )
+   assert( *x1 != NULL );
+   assert( *x2 != NULL );
+
+   if( is_zero( a ) )
       {
-      if( isZero( b ) )
+      if( is_zero( b ) )
         {
-        if( isZero( c ) )  return -1;      //a = 0 && b = 0 && c = 0
+        if( is_zero( c ) )  return -1;      //a = 0 && b = 0 && c = 0
 
         else               return 0;       //a = 0 && b = 0 && c != 0
         }
@@ -95,42 +98,40 @@ int solve_of_qua_equ( double a, double b, double c, double* x1, double* x2)
       {
       Discr = b * b - 4 * a * c;
 
-      if( Discr > 0 )
-        {
-        *x1 = ( -b + sqrt( Discr ) ) / 2;
-        *x2 = ( -b - sqrt( Discr ) ) / 2;
-        return 2;
-        }
-
-      if( Discr == 0 )
-        {
-        *x2 = *x1 = ( -b + sqrt( Discr ) ) / 2;
-        return 1;
-        }
-
       if( Discr < 0 ) return 0;
-      }
+
+      else
+        {
+      *x2 = ( -b - sqrt( Discr ) ) / 2;
+      *x1 = ( -b + sqrt( Discr ) ) / 2;
+
+      if( Discr > 0 )  return 2;
+
+      if( Discr == 0 ) return 1;
+         }
+
+       }
     }
 
 //--------------------------------------------------------------------------------------------
 
-int isZero( double Value )
+int is_zero( double Value )
   {
-  return ( fabs( Value ) < bias ) ;
+  return( fabs( Value ) < bias ) ;
   }
 
 //-----------------------------------------------------------------------------------------
 
-void test_isZero()
+void test_is_zero()
   {
-  if( isZero( 0 ) == 1 )   printf( "Test one Yes \n" );
-  else                     printf( "Test one No: result = %d, should be 1\n", isZero( 0 ) );
+  if( is_zero( 0 ) == 1 )   printf( "Test one Yes \n" );
+  else                     printf( "Test one No: result = %d, should be 1\n", is_zero( 0 ) );
 
-  if( isZero( 1 ) == 0 )   printf( "Test two Yes \n" );
-  else                     printf( "Test two No: result = %d, should be 0 \n", isZero( 1 ) );
+  if( is_zero( 1 ) == 0 )   printf( "Test two Yes \n" );
+  else                     printf( "Test two No: result = %d, should be 0 \n", is_zero( 1 ) );
 
-  if( isZero( bias ) == 0) printf( "Test third Yes \n" );
-  else                     printf( "Test third No: result = %d, should be 0 \n", isZero( bias ) );
+  if( is_zero( bias ) == 0) printf( "Test third Yes \n" );
+  else                     printf( "Test third No: result = %d, should be 0 \n", is_zero( bias ) );
   }
 
 //------------------------------------------------------------------------------------------------
